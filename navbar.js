96c5e4236e0a78ed39b49cc7e71ae320eee14f8c
Brett McDowell <144458187+BrettMcD24@users.noreply.github.com>
2025-11-17 22:02:18 +0000

    navbar option/test

--- navbar.js
+++ navbar.js
@@ -52,3 +52,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Would also work:
+// document.addEventListener("DOMContentLoaded", function () {
+//   var nav = document.getElementById("main-nav");
+//   if (!nav) return;
+
+//   nav.innerHTML =
+//     '<nav class="navbar">' +
+//     '<div class="nav-brand"><a href="index.html">COPE &amp; Climbing</a></div>' +
+//     '<ul class="nav-links">' +
+//     '<li><a href="index.html">Home</a></li>' +
+//     '<li><a href="standards.html">Standards</a></li>' +
+//     '<li><a href="classTopics.html">Class Topics</a></li>' +
+//     '<li><a href="exam.html">Exam</a></li>' +
+//     "</ul>" +
+//     "</nav>";
+// });
