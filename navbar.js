ccb4c5d98728b08fe3d36ea9b0743348097328b3
William Petrik <87589017+bpp123456789@users.noreply.github.com>
2025-11-18 23:53:29 +0000

    mike changes

--- navbar.js
+++ navbar.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
     '<nav class="navbar">' +
     '<div class="nav-brand"><a href="index.html">COPE &amp; Climbing</a></div>' +
     '<ul class="nav-links">' +
-    '<li><a href="index.html">Home</a></li>' +
     '<li><a href="standards.html">Standards</a></li>' +
     '<li><a href="classTopics.html">Class Topics</a></li>' +
     '<li><a href="exam.html">Exam</a></li>' +
